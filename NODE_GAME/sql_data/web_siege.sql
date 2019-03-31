05f1ef53ff9aa0405cc28d4043522a1422674226
Kamil <kamil.witonski@o2.pl>
2019-03-31 15:27:36 +0100

    add guns table to database

--- NODE_GAME/sql_data/web_siege.sql
+++ NODE_GAME/sql_data/web_siege.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 31, 2019 at 03:24 PM
+-- Generation Time: Mar 31, 2019 at 04:27 PM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -21,6 +21,32 @@ SET time_zone = "+00:00";
 --
 -- Database: `web_siege`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `guns`
+--
+
+CREATE TABLE `guns` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `sprite` varchar(100) NOT NULL,
+  `fire_rate` int(11) NOT NULL,
+  `reload_time` int(11) NOT NULL,
+  `bullet_velocity` int(11) NOT NULL,
+  `bullet_damage` int(11) NOT NULL,
+  `mag_size` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `guns`
+--
+
+INSERT INTO `guns` (`id`, `name`, `sprite`, `fire_rate`, `reload_time`, `bullet_velocity`, `bullet_damage`, `mag_size`) VALUES
+(1, 'pistol', '', 300, 2000, 20, 25, 7),
+(2, 'UZI', '', 150, 2500, 40, 30, 15),
+(3, 'AK47', '', 200, 2500, 45, 33, 30);
 
 -- --------------------------------------------------------
 
@@ -48,6 +74,12 @@ INSERT INTO `maps` (`id`, `name`, `tile_map`, `tile_set`) VALUES
 --
 
 --
+-- Indexes for table `guns`
+--
+ALTER TABLE `guns`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `maps`
 --
 ALTER TABLE `maps`
@@ -56,6 +88,12 @@ ALTER TABLE `maps`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `guns`
+--
+ALTER TABLE `guns`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `maps`
