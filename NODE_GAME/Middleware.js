d48ea181092f00042aa7519d0e15823d32d642a1
Kamil <kamil.witonski@o2.pl>
2019-04-18 20:29:12 +0100

    add middleware to check login status on routes

---
+++ NODE_GAME/Middleware.js
@@ -0,0 +1,18 @@
+var Middleware = {
+	isLoggedIn: function(req, res, next) {
+	    console.log('lslslslsl');
+
+	    if(req.isAuthenticated()) {
+	    	console.log('logged in');
+	        res.locals.isLoggedIn = true;
+	        return next();
+	    } else {
+	    	console.log('NOT LOGGED IN');
+	        res.locals.isLoggedIn = false;
+
+	        res.redirect('/login');
+	    }
+	}
+}
+
+module.exports = Middleware;
