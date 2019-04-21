bfff8ed22b0fa9ca980540c15798090bad819fee
Kamil <kamil.witonski@o2.pl>
2019-04-21 13:53:35 +0100

    update menu screen

--- NODE_GAME/public/js/menu.js
+++ NODE_GAME/public/js/menu.js
@@ -3,30 +3,44 @@
 var menuState = {
 
     create: function () {
-		
+		// this.game.state.restart();
+
+        console.log('menu!?');
+
 		//Background
 		game.add.tileSprite(0, 0, 1000, 600, 'menubg');
 
         //Create string for title
-        var nameLabel = game.add.text(80, 80, 'Screen for User account', { font: '25px Arial', fill: '#ffffff' })
+        var nameLabel = game.add.text(80, 80, 'Web Siege', { font: '25px Arial', fill: '#ffffff' })
 
         //Create string of instructions
-        var startLabel = game.add.text(80, game.world.height-80, 'Log In and Press W to start', { font: '25px Arial', fill: '#ffffff' })
+        var test = game.add.text(80, 370, 'Press W to join game', { font: '25px Arial', fill: '#ffffff' })
+        var test1 = game.add.text(80, 400, 'Press S for stats', { font: '25px Arial', fill: '#ffffff' })
 		
+        test.bringToTop();
+
+
+        console.log(test);
+
 		//Create Music
 		var music = game.add.audio('menumusic');
 		
 		//Set Volume
-		music.volume = 0.1;
+		music.volume = 0.1; 
 		
 		//Play Music
-		music.play();
-		
+		// music.play();
+
+
+        var skey = game.input.keyboard.addKey(Phaser.Keyboard.S);
+
+        skey.onDown.addOnce(this.showStats, this);
+
         //Create initate the W key
         var wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
 
         //When W key is pressed function is called.
-        wkey.onDown.addOnce(this.start, this,);
+        wkey.onDown.addOnce(this.start, this);
     },
 
     //Start function
@@ -39,5 +53,8 @@ var menuState = {
         game.state.start('play');
 		
     },
+    showStats: function() {
+        game.state.start('stats');
+    }
 };
 
