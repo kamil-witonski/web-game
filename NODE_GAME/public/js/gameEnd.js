fac8d1c0a59be2508760431b330fee9935fc9349
Kamil <kamil.witonski@o2.pl>
2019-04-21 13:52:18 +0100

    fix end screen

--- NODE_GAME/public/js/gameEnd.js
+++ NODE_GAME/public/js/gameEnd.js
@@ -1,29 +1,33 @@
 // JavaScript source code for loadState
-
+// var countdown;
+var count = 10;
 var gameEnd = {
+    countdown: "",
     data: "",
     init: function(data) {
         this.data = data;
+        count = 10;
         console.log(data);
     },
     //Load all assets for the menu
     preload: function () {
         //Create loading text
         // var loadlingLabel = game.add.text(80, 140, "display table?...", { font: "30px Courier", fill: "#ffffff" });
-        var winner = game.add.text(90, 150, "Winner: " + this.data.winner.user_id, { font: "30px Courier", fill: "#ffffff" });
+        var winner = game.add.text(90, 140, "Winner: " + this.data.winner.username, { font: "30px Courier", fill: "#ffffff" });
+        this.countdown = game.add.text(90, 400, "Next level in " + count, { font: "30px Courier", fill: "#ffffff" });
 
         this.createTable();
     },
     create: function () {
         console.log("we are in eng game?");
-
         var selfs = this;
-        var count = 30;
-
+        
         //count down before game restarts
         var timer = setInterval(function() {
-
             count--;
+
+            selfs.countdown.text =  "Next level in " + count;
+
             if(count == 1) {
                 clearInterval(timer);
                 //clear players array
@@ -37,8 +41,8 @@ var gameEnd = {
         var fontStyle = { font: "20px Courier", fill: "#ffffff" };
         var players = this.data.players;
 
-        game.add.text(200, 180, "POS", fontStyle);
-        game.add.text(270, 180, "NAME", fontStyle);
+        game.add.text(180, 180, "POS", fontStyle);
+        game.add.text(260, 180, "NAME", fontStyle);
         game.add.text(380, 180, "KILLS", fontStyle);
         game.add.text(470, 180, "DEATHS", fontStyle);
         game.add.text(560, 180, "DAMAGE", fontStyle);
@@ -47,9 +51,11 @@ var gameEnd = {
 
         var yOffset = 200;
 
+        console.log(players);
+
         $.each(players, function(i, player) {
-            game.add.text(210, yOffset, count++, fontStyle);
-            game.add.text(280, yOffset, "NAME", fontStyle);
+            game.add.text(190, yOffset, count++, fontStyle);
+            game.add.text(270, yOffset, player.username, fontStyle);
             game.add.text(390, yOffset, player.kills, fontStyle);
             game.add.text(480, yOffset, player.deaths, fontStyle);
             game.add.text(570, yOffset, player.damageDelt, fontStyle);
