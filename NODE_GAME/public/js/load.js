390011bfacbc51937af437e8396eed0c969b9934
Kamil <kamil.witonski@o2.pl>
2019-04-21 13:53:08 +0100

    fix load menu
    omit pre-menu as its not required

--- NODE_GAME/public/js/load.js
+++ NODE_GAME/public/js/load.js
@@ -23,7 +23,6 @@ var loadState = {
         this.load.spritesheet('recruit_legs', '/public/assets/sprites/rec_legs.png', 61, 76);
         this.load.spritesheet('recruit_body', '/public/assets/sprites/rec_body.png', 61, 76);
 
-
         this.load.tilemap('test_map', '/public/assets/test_map/test_map.json', null, Phaser.Tilemap.TILED_JSON);
         this.load.spritesheet('tileset1', '/public/assets/test_map/5z1KX.png', 32, 32);
 
@@ -35,35 +34,7 @@ var loadState = {
         this.load.audio('uzi', ['/public/assets/audio/barreta.mp3']);
         this.load.audio('rifle', ['/public/assets/audio/m4a1.mp3']);
 		this.load.audio('sniper', ['/public/assets/audio/sniper.mp3']);
-        // this.load.audio('pistol', ['/public/assets/audio/pistol.mp3']);
-		
-		//I wonder if we can group this data and call it as a whole
-		
-/* 		//Load level1 (group 1)
-		this.load.image('lvl1-sky', '/public/assets/level_1/level1_sky.png');
-		this.load.image('lvl1-cloudstr', 'public/assets/level_1/level1_cloudstr.png');
-		this.load.image('lvl1-clouds', '/public/assets/level_1/level1_clouds.png');
-		this.load.image('lvl1-backtrees', '/public/assets/level1/level_backtrees.png');
-		this.load.image('lvl1-trees', '/public/assets/level1/level1_trees.png'); 
-		
-		//Load level2 (group 2)
-		this.load.image('lvl2-sky', '/public/assets/level_2/level2_sky.png');
-		this.load.image('lvl2-cloudstr', 'public/assets/level_2/level2_cloudstr.png');
-		this.load.image('lvl2-clouds', '/public/assets/level_2/level2_clouds.png');
-		this.load.image('lvl2-backtrees', '/public/assets/level_2/level2_backtrees.png');
-		this.load.image('lvl2-trees', '/public/assets/level_2/level2_trees.png');
-		this.load.image('lvl2-midtrees1', '/public/assets/level_2/level2_midtrees1.png');
-		this.load.image('lvl2-midtrees2', '/public/assets/level_2/level2_midtrees2.png');
-		
-		//Load level3 (group 3)
-		this.load.image('lvl3-sky', '/public/assets/level_3/level3_sky.png');
-		this.load.image('lvl3-cloudstr', 'public/assets/level_3/level3_cloudstr.png');
-		this.load.image('lvl3-trees', '/public/assets/level_3/level3_trees.png');
-		this.load.image('lvl3-background', '/public/assets/level_3/level3_background.png');
-		this.load.image('lvl3-midground', '/public/assets/level_3/level3_midground.png');
-		this.load.image('lvl3-foreground', '/public/assets/level_3/level3_foreground.png');
-*/
-
+	
         //load each gun dynamically for the database
         $.ajax({
             type: 'GET',
@@ -76,7 +47,6 @@ var loadState = {
                     game.load.image(gun.name + '_gun', gun.asset_img);
 
                 });
-               
             }
         });
 
@@ -86,8 +56,9 @@ var loadState = {
         this.sound.add('pistol');
         this.sound.add('uzi');
         this.sound.add('rifle');
+        this.sound.add('sniper');
 
-        game.state.start("pre-menu");
+        game.state.start("menu");
     },
 };
 
